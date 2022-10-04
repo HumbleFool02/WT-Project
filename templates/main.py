217b93275843bb508a57a821b5962e179f2816c4
Piyush Kasle <kaslepiyush07@gmail.com>
2022-10-05 01:22:54 +0530

    New changes

--- templates/main.py
+++ templates/main.py
@@ -7,15 +7,18 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/")
-def about():
-    return render_template('about.html')
+@app.route("/ManagerLogin")
+def ManagerLogin():
+    return render_template('ManagerLogin.html')
 
 
-@app.route("/contact")
-def contact():
-    return render_template('contact.html')
+@app.route("/SecurityLogin")
+def SecurityLogin():
+    return render_template('SecurityLogin.html')
 
+@app.route("/Registration")
+def registration():
+    return render_template('registration.html')
 
 app.run(debug=True)
 
