7b624354c09b77e498d84d19b880c19e18451477
Hardik7843 <84066682+Hardik7843@users.noreply.github.com>
2022-10-06 19:28:28 +0530

    commitfrom hardik's laptop

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ from flask import request
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/securitysystem'
-db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/onlinesystem'
+db = SQLAlchemy(app)
 
-class Contacts(db.Model):
+class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(20), unique=False, nullable=False)
+    phone = db.Column(db.String(20), unique=False, nullable=False)
     msg = db.Column(db.String(120), unique=False, nullable=False)
 
+class Registration(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    domain = db.Column(db.String(120), unique=False, nullable=False)
+    idno = db.Column(db.String(120), unique=True, nullable=False)
+    pword = db.Column(db.String(120), unique=False, nullable=False)
 
 @app.route("/")
 def home():
@@ -31,8 +39,18 @@ def ManagerLogin():
 def SecurityLogin():
     return render_template('SecurityLogin.html')
 
-@app.route("/registration")
+@app.route("/registration", methods = ['GET', 'POST'])
 def registration():
+    if(request.method=='POST'):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        username = request.form.get('username')
+        domain = request.form.get('domain')
+        idno = request.form.get('idno')
+        pword = request.form.get('pword')
+        entry = Registration(name=name, username = username , domain = domain ,idno = idno, pword = pword)
+        db.session.add(entry)
+        db.session.commit()
     return render_template('registration.html')
 
 @app.route("/contact", methods = ['GET', 'POST'])
@@ -41,8 +59,9 @@ def contact():
         '''Add entry to the database'''
         name = request.form.get('name')
         email = request.form.get('email')
+        phone = request.form.get('phone')
         message = request.form.get('msg')
-        entry = Contacts(name=name, email = email , msg = message)
+        entry = Contact(name=name, email = email ,phone = phone, msg = message)
         db.session.add(entry)
         db.session.commit()
     return render_template('contact.html')    
